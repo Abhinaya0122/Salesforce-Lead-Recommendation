4d501dffc3e83a2a67485d15460daefb27c135e6
Abhinaya Shrinivasan <126884981+Abhinaya0122@users.noreply.github.com>
2025-01-08 20:38:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,3 @@ def predict():
     except Exception as e:
         print(f"Error during prediction: {e}")
         return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
